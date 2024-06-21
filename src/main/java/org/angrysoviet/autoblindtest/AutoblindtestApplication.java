ffd710ca9e51e03d36fc07a5e9ba5131706ba735
Hugo MOREAU [8ff749c8-0e41-4edd-a491-4067a3d81970] <hugo.moreau@ca-ps.com>
2024-06-21 20:11:28 +0200

    Initial commit

---
+++ src/main/java/org/angrysoviet/autoblindtest/AutoblindtestApplication.java
@@ -0,0 +1,13 @@
+package org.angrysoviet.autoblindtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoblindtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoblindtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/angrysoviet/autoblindtest/AutoblindtestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.angrysoviet.autoblindtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoblindtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
